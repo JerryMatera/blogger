e1b6b7e0b1e10b48427242e0792372c726a449ba
JerryMatera <jerryonyango8@gmail.com>
2023-01-07 21:32:42 +0300

    update question 2

--- lib/uppercase.ex
+++ lib/uppercase.ex
@@ -1,5 +1,9 @@
 defmodule Uppercase do
   def uppercase(list) do
-    Enum.map(list, fn x -> String.upcase(x) end)
+    cond do
+      is_list(list) -> Enum.map(list, fn x -> String.upcase(x) end)
+      is_bitstring(list) -> String.upcase(list)
+      true -> IO.puts("Input a string or list of string")
+    end
   end
 end
